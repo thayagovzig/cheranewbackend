0dfda02ac01545fc3f0bc113c122ce1ac475a021
Thaya Govzig <thaya.govzig2101@gmail.com>
2025-07-05 07:13:47 +0200

    Update origins

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://156.67.111.97',
+  origin: ['http://156.67.111.97', 'http://chera.futoflex.com', 'https://chera.futoflex.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
